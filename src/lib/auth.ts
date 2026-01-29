17a81a7abff41b02bfce531593250c3aa60e9792
Thomas Testa <ttesta99@yahoo.com>
2026-01-29 16:36:35 -0500

    implement database and auth schemas

---
+++ src/lib/auth.ts
@@ -0,0 +1,16 @@
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";// your drizzle instance
+import { db } from "../db";
+import * as schema from '../db/schema/auth'
+
+export const auth = betterAuth({
+    secret: process.env.BETTER_AUTH_SECRET!, 
+    trustedOrigins: [process.env.FRONTEND_URL!], 
+    database: drizzleAdapter(db, {
+        provider: "pg", 
+        schema
+    }),
+    emailAndPassword: {
+        enabled: true, 
+    }
+})
