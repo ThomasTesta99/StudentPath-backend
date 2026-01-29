e1c725a524b85f33206c9da5942bf56e04c1e1a7
Thomas Testa <ttesta99@yahoo.com>
2026-01-29 17:26:12 -0500

    add cors and auth route

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,22 @@
 import express from 'express'
+import cors from 'cors'
+import { toNodeHandler } from "better-auth/node";
+import { auth } from './lib/auth';
 
 const app = express();
 const PORT = 8000;
+
+if(!process.env.FRONTEND_URL){
+    throw new Error("FRONTEND_URL not defined");
+}
+
+app.use(cors({
+    origin: process.env.FRONTEND_URL, 
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true, 
+}));
+
+app.all("/api/auth/*", toNodeHandler(auth));
 
 app.use(express.json());
 
