ac2f24117ca6e50d2a034e133efbe5db777a3778
Thomas Testa <ttesta99@yahoo.com>
2026-02-04 12:56:47 -0500

    implement term table

--- src/db/schema/app.ts
+++ src/db/schema/app.ts
@@ -1,4 +1,4 @@
-import { pgTable, text, timestamp } from "drizzle-orm/pg-core";
+import { boolean, pgTable, text, timestamp } from "drizzle-orm/pg-core";
 
 const timestamps = {
     createdAt: timestamp('created_at').defaultNow().notNull(),
@@ -6,7 +6,17 @@ const timestamps = {
 };
 
 export const schools = pgTable("schools", {
-    schoolId: text("school_id").primaryKey(),
+    id: text("id").primaryKey(),
     schoolName: text("school_name").notNull(),
+    ...timestamps
+});
+
+export const terms = pgTable("terms", {
+    id: text(" id").primaryKey(), 
+    schoolId: text("school_id").references(() => schools.id, {onDelete: "cascade"}), 
+    termName: text("term_name"),
+    startDate: timestamp("start_date").notNull(), 
+    endDate: timestamp("end_date").notNull(), 
+    isActive: boolean("is_active").default(false),
     ...timestamps
 })
