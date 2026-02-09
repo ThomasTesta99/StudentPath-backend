db193b18dd56d28cb428c4a8cb5a0df145c3d343
Thomas Testa <ttesta99@yahoo.com>
2026-02-09 18:05:24 -0500

    implement patch courses route

--- src/routes/admin/courses.ts
+++ src/routes/admin/courses.ts
@@ -1,5 +1,5 @@
 import express from "express"
-import { courses, departments, NewCourse, schools, terms, user } from "../../db/schema";
+import { courses, departments, NewCourse, schools, teacherProfiles, terms, user } from "../../db/schema";
 import { randomUUID } from "crypto";
 import { db } from "../../db";
 import { and, desc, eq, getTableColumns, ilike, or, sql } from "drizzle-orm";
@@ -192,5 +192,62 @@ coursesRouter.get("/:id", async (req, res) => {
     } catch (error) {
         console.error("GET /courses/id error: ", error);
         return res.status(500).json({error: "There was an error getting the course"});
+    }
+})
+
+coursesRouter.patch("/:id", async (req, res) => {
+    try {
+        const {id} = req.params;
+        const {termId, teacherId, departmentId, name, gradeLevel, code, description} = req.body;
+
+        const updates: Partial<NewCourse> = {};
+
+        if(typeof termId === "string" && termId.trim().length > 0) updates.termId = termId.trim();
+        if(typeof teacherId === "string" && teacherId.trim().length > 0) updates.teacherId = teacherId.trim();
+        if(typeof departmentId === "string" && departmentId.trim().length > 0) updates.departmentId = departmentId.trim();
+        if(typeof name === "string" && name.trim().length > 0) updates.name = name.trim();
+        if(typeof gradeLevel === "string" && gradeLevel.trim().length > 0) updates.gradeLevel = gradeLevel.trim();
+        if(typeof code === "string" && code.trim().length > 0) updates.code = code.trim();
+        if(typeof description === "string"  && description.trim().length > 0) updates.description = description.trim();
+
+        if(Object.keys(updates).length === 0){
+            return res.status(400).json({error: "No valid fields to update"});
+        }
+
+        const [existingCourse] = await db
+            .select({id: courses.id})
+            .from(courses)
+            .where(eq(courses.id, id))
+            .limit(1);
+
+        if(!existingCourse) return res.status(404).json({error: "Course not found"});
+
+        if(updates.termId){
+            const [term] = await db.select({id: terms.id}).from(terms).where(eq(terms.id, updates.termId)).limit(1);;
+            if(!term) return res.status(404).json({error: "invalid Term"});
+        }
+
+        if(updates.departmentId){
+            const [department] = await db.select({id: departments.id}).from(departments).where(eq(departments.id, updates.departmentId)).limit(1);;
+            if(!department) return res.status(404).json({error: "invalid department"});
+        }
+
+        if(updates.teacherId){
+            const [teacher] = await db.select({id: teacherProfiles.userId}).from(teacherProfiles).where(eq(teacherProfiles.userId, updates.teacherId)).limit(1);;
+            if(!teacher) return res.status(404).json({error: "Invalid teacher"})
+        }
+
+        const [updated] = await db
+            .update(courses)
+            .set(updates)
+            .where(eq(courses.id, id))
+            .returning();
+
+        if(!updated) return res.status(400).json({error: "There was an error updating the course"});
+
+        return res.status(200).json({course: updated});
+    } catch (error) {
+        console.error("PATCH /courses error: ", error);
+        return res.status(500).json({error: "There was an error updating the course"});
     }
 })
